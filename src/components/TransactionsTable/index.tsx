10231dd89869307383e7539b67bd62dbdb063ef7
kleber <kleber099@gmail.com>
2021-04-29 21:54:03 -0300

    Layout e componente da aplicaçao

---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,40 @@
+import { useEffect } from "react";
+import { api } from "../../services/api";
+
+import { Container } from "./styles";
+
+export function TransactionsTable() {
+  useEffect(() => {
+    api('transactions')
+      .then(response => console.log(response.data))
+  }, []);
+
+  return (
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Título</th>
+            <th>Valor</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Desenvolvimento de web site</td>
+            <td className="deposit">R$12.0000</td>
+            <td>Desenvolvimento</td>
+            <td>20/02/2021</td>
+          </tr>
+          <tr>
+            <td>Desenvolvimento de web site</td>
+            <td className="withdraw">- R$1.100</td>
+            <td>Casa</td>
+            <td>17/02/2021</td>
+          </tr>
+        </tbody>
+      </table>
+    </Container>
+  );
+}
