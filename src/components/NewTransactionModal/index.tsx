d62868aae06adc4820186940a2a3f5efc38c2519
kleber <kleber099@gmail.com>
2021-05-01 20:58:39 -0300

    Criando os componente de buttoes de entrada e saida

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,8 @@
 import Modal from 'react-modal';
 import closeImg from '../../assets/close.svg';
-import { Container } from './styles';
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
+import { Container, TransactionTypeContainer } from './styles';
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -23,10 +25,21 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
         <img src={closeImg} alt="Fechar modal"/>
       </button>
       <Container>
-        <h2>Cadastrar transação</h2>
+        <h2>Cadastrar Transação</h2>
 
         <input placeholder="Título" />
         <input type="number" />
+        <TransactionTypeContainer>
+          <button type="button">
+              <img src={incomeImg} alt="Entrada"></img>
+              <span>Entrada</span>
+          </button>
+
+          <button type="button">
+              <img src={outcomeImg} alt="Saída"></img>
+              <span>Saída</span>
+          </button>
+        </TransactionTypeContainer>
         <input placeholder="Categoria" />
         <button type="submit">
           Cadastrar
