c6b5a33dd4192669c0927cc11fa0c30c200a74ec
kleber <kleber099@gmail.com>
2021-05-08 11:08:10 -0300

    NEW: APlicando contexto no react, centralizando os dados das transaçoes no contexto

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Modal from 'react-modal';
 import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
 import {  NewTransactionModal } from './components/NewTransactionModal';
+import { TransactionsProvider } from './hooks/useTransactions';
 
 import { GlobalStyle } from './styles/global'
 
@@ -21,7 +22,7 @@ export function App() {
   }
 
   return (
-    <div className="App">
+    <TransactionsProvider>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
       <Dashboard />
       <NewTransactionModal 
@@ -29,6 +30,6 @@ export function App() {
         onRequestClose={handleCloseNewTransactionModal} 
       />
       <GlobalStyle />
-    </div>
+    </TransactionsProvider>
   );
 }
